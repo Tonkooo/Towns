d96df8cb431246d5e3630a880be188d58aeeb46e
Tonkooo <at.antonio.todorov@gmail.com>
2023-11-20 06:43:22 +0200

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -28,3 +28,25 @@ function addTown() {
 	$('#towns').append($('<option>').text(townName));
 	$('#result').text(townName + " added.");
 }
+function deleteTown() {
+    let townName = $('#townName').val();
+    $('#townName').val('');
+    let removed = false;
+    for (let option of $('#towns option')) {
+        if (option.textContent == townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+    if (removed)
+        showMessage(townName + " deleted.");
+    else
+        showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+    $('#result').text(msg).css("display", "block");
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
+}
